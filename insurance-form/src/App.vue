ca600039223570e7e6d637a2cc56a860488563c7
Azhar <azhar0287@gmail.com>
2022-03-11 01:10:55 +0500

    changes in navbar

--- insurance-form/src/App.vue
+++ insurance-form/src/App.vue
@@ -8,9 +8,20 @@
 
         
       <b-navbar-nav class="ml-auto">
-         <b-nav-form>
+        <b-nav-form>
+          <b-nav-item class="app-nav-link active md my-2 my-sm-0" :to="{ name: 'insurance' }">Create Order</b-nav-item>               
+        </b-nav-form>
+        
+        <b-nav-form>
+          <b-nav-item class="app-nav-link active md my-2 my-sm-0" :to="{ name: 'patientCount' }">Patient Count</b-nav-item>               
+        </b-nav-form>
+
+        
+        <b-nav-form>
           <b-nav-item class="app-nav-link active md my-2 my-sm-0" :to="{ name: 'dashboard' }">Dashboard</b-nav-item>               
         </b-nav-form>
+
+
       </b-navbar-nav>
 
       </b-navbar>
